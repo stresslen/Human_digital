05dfed33d0278f29d5eeebffa64debe834c3f403
stresslen <dinhdat16.02.2005@gmail.com>
2025-09-21 13:22:49 +0700

    second

--- datasetsss.py
+++ datasetsss.py
@@ -14,7 +14,7 @@ class MyDataset(Dataset):
     
         self.img_path_list = []
         self.lms_path_list = []
-        self.mode = mode  # wenet or hubert
+        self.mode = mode
         
         for i in range(len(os.listdir(img_dir+"/full_body_img/"))):
 
@@ -29,13 +29,18 @@ class MyDataset(Dataset):
             self.audio_feats = np.load(img_dir+"/aud_hu.npy")
             
         self.audio_feats = self.audio_feats.astype(np.float32)
+        print(img_dir)
+        print(self.audio_feats.shape)
+        print(len(self.img_path_list))
         
     def __len__(self):
-        return self.audio_feats.shape[0] if self.audio_feats[0]<len(self.img_path_list) else len(self.img_path_list)
+        # return len(self.img_path_list)-1
+        # return len(self.img_path_list)
+        return self.audio_feats.shape[0]-1
     
-    def get_audio_features(self, features, index):  # 在当前音频帧前后各取4帧音频特征
-        left = index - 4
-        right = index + 4
+    def get_audio_features(self, features, index):
+        left = index - 8
+        right = index + 8
         pad_left = 0
         pad_right = 0
         if left < 0:
@@ -51,6 +56,19 @@ class MyDataset(Dataset):
             auds = torch.cat([auds, torch.zeros_like(auds[:pad_right])], dim=0) # [8, 16]
         return auds
     
+    def get_audio_features_1(self, features, index):
+    
+        left = index - 8
+        pad_left = 0
+        if left < 0:
+            pad_left = -left
+            left = 0
+        auds = features[left:index]
+        auds = torch.from_numpy(auds)
+        if pad_left > 0:
+            # pad may be longer than auds, so do not use zeros_like
+            auds = torch.cat([torch.zeros(pad_left, *auds.shape[1:], device=auds.device, dtype=auds.dtype), auds], dim=0)
+        return auds
     
     def process_img(self, img, lms_path, img_ex, lms_path_ex):
 
@@ -61,7 +79,7 @@ class MyDataset(Dataset):
                 arr = line.split(" ")
                 arr = np.array(arr, dtype=np.float32)
                 lms_list.append(arr)
-        lms = np.array(lms_list, dtype=np.int32)  # 关键点坐标
+        lms = np.array(lms_list, dtype=np.int32)
         xmin = lms[1][0]
         ymin = lms[52][1]
         
@@ -69,14 +87,12 @@ class MyDataset(Dataset):
         width = xmax - xmin
         ymax = ymin + width
         
-        crop_img = img[ymin:ymax, xmin:xmax] # 将人脸下半部分区域裁切出来
-        crop_img = cv2.resize(crop_img, (168, 168), cv2.INTER_AREA) 
-        # resize后保留边缘的4个像素，如果视频分辨率比较大的话 建议把resize值和这个值都改大 但宽高必须能被16整除，同时模型结构也要改
-        img_real = crop_img[4:164, 4:164].copy() # 保留边缘的4个像素防止贴回去的时候比较违和
+        crop_img = img[ymin:ymax, xmin:xmax]
+        crop_img = cv2.resize(crop_img, (168, 168), cv2.INTER_AREA)
+        img_real = crop_img[4:164, 4:164].copy()
         img_real_ori = img_real.copy()
-        img_masked = cv2.rectangle(img_real,(5,5,150,145),(0,0,0),-1) # 将图片中间区域涂黑
+        img_masked = cv2.rectangle(img_real,(5,5,150,145),(0,0,0),-1)
         
-        # 取一张随机图像作为参考和要做推理的图像一起输入 ⬇️⬇️⬇️
         lms_list = []
         with open(lms_path_ex, "r") as f:
             lines = f.read().splitlines()
@@ -92,7 +108,7 @@ class MyDataset(Dataset):
         width = xmax - xmin
         ymax = ymin + width
         crop_img = img_ex[ymin:ymax, xmin:xmax]
-        crop_img = cv2.resize(crop_img, (168, 168), cv2.INTER_AREA) 
+        crop_img = cv2.resize(crop_img, (168, 168), cv2.INTER_AREA)
         img_real_ex = crop_img[4:164, 4:164].copy()
         
         img_real_ori = img_real_ori.transpose(2,0,1).astype(np.float32)
@@ -114,13 +130,13 @@ class MyDataset(Dataset):
         img_ex = cv2.imread(self.img_path_list[ex_int])
         lms_path_ex = self.lms_path_list[ex_int]
         
-        img_concat_T, img_real_T = self.process_img(img, lms_path, img_ex, lms_path_ex) ## 图像处理
-        audio_feat = self.get_audio_features(self.audio_feats, idx)  ## 音频特征处理
+        img_concat_T, img_real_T = self.process_img(img, lms_path, img_ex, lms_path_ex)
+        audio_feat = self.get_audio_features(self.audio_feats, idx) 
         
         if self.mode == "wenet":
-            audio_feat = audio_feat.reshape(128,16,32)
+            audio_feat = audio_feat.reshape(256,16,32)
         if self.mode == "hubert":
-            audio_feat = audio_feat.reshape(16,32,32)  ## 这个地方的16 / 128跟合并起来的音频特征帧数有关
+            audio_feat = audio_feat.reshape(32,32,32)
         
         return img_concat_T, img_real_T, audio_feat
     
